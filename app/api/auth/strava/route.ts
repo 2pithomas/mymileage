f45bdde984a69d80ec6df5211aadc2007e9bce5a
2pibacon <thomasbacon@email.arizona.edu>
2026-09-12 20:46:01 -0500

    feat(auth): ensuring read all

--- app/api/auth/strava/route.ts
+++ app/api/auth/strava/route.ts
@@ -2,10 +2,13 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const appUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://www.mymileage.cc';
-  const redirectUri = `${appUrl}/api/auth/strava/callback`;
+  const clientId = process.env.STRAVA_CLIENT_ID;
+  const redirectUri = encodeURIComponent(
+    `${process.env.NEXT_PUBLIC_APP_URL || 'https://www.mymileage.cc'}/api/auth/strava/callback`
+  );
 
-  const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${process.env.STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&approval_prompt=force&scope=read,activity:read_all,activity:write`;
+  // IMPORTANT: scope MUST contain activity:read_all to unlock athlete gear (bikes & shoes)
+  const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&approval_prompt=force&scope=read,activity:read_all`;
 
   return NextResponse.redirect(stravaAuthUrl);
 }
